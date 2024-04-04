9c3bff9505644cc713fe92fe9618290562ddb8f3
Serhii Gramma <grammas56@gmail.com>
2024-04-04 13:03:14 +0300

    figures resized 1.3

--- src/components /LostFigures.tsx
+++ src/components /LostFigures.tsx
@@ -19,7 +19,7 @@ const LostFigures: FC<LostFiguresProps> = ({title, figures, setGameOver}) => {
       <h3>{title}</h3>
       {figures.map(figure =>
         <div key={figure.id}>
-          {figure.name} {figure.logo && <img width={20} height={20} src={figure.logo}/>}
+          {figure.name} {figure.logo && <img width={10} height={10} src={figure.logo}/>}
         </div>
       )}
     </div>
